9c305cd9c0d2978bca89611c12c8138dd718a652
Jinhyejeong <149468613+Jinhyejeong@users.noreply.github.com>
2023-11-06 20:09:37 +0900

    추가

--- 3.c
+++ 3.c
@@ -1,7 +1,7 @@
 #include “myProject.h"
 int printMyName4(void) //1~4중 하나. 파일이름에 맞게 할 것
 {
-printf (“4. my name is [송종욱]\n”); //자기 이름
+printf (“3. my name is [송종욱]\n”); //자기 이름
 return 1;
 }
 
--- myProject.h
+++ myProject.h
@@ -2,4 +2,6 @@
 #define MYPROJECT_H
 
 int printMyname1(void);
+int printMyname2(void);
+int printMyname3(void);
 #endif
