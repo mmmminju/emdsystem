7cb753c696b491cc5a9ae54f545bf79fad47428a
Parkjaesung1 <su0000ng@naver.com>
2023-11-06 16:03:32 +0900

    추가
    추가

---
+++ 1.c
@@ -0,0 +1,6 @@
+#include "myProject.h"
+int printMyname1(void)
+{
+	printf("1. my name is [박재성]\n);
+	return 1;
+}
--- myProoject.h
+++ myProoject.h
@@ -1,5 +1,5 @@
 #ifndef MYPROJECT_H
 #define MYPROJECT_H
 
-
+int printMyname1(void);
 #endif
