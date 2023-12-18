6a0b615a5675202bcfddd6d1d69b22d8e4deb779
Parkjaesung1 <su0000ng@naver.com>
2023-12-18 17:03:24 +0900

    정리

---
+++ project/Device_test.c
@@ -0,0 +1,527 @@
+#include <math.h>
+#include <stdlib.h> // for exit
+#include <pthread.h>
+#include "./lib/accelMagGyro.h"
+#include "./lib/bitmap.h"
+#include "./lib/button.h"
+#include "./lib/buzzer.h"
+#include "./lib/colorLED.h"
+#include "./lib/fnd.h"
+#include "./lib/led.h"
+#include "./lib/temp.h"
+#include "./lib/textLCD.h"
+#include "./lib/display.h"
+
+#define MENU 7
+#define WIN 8
+#define PWROFF 9
+#define FAIL 10
+#define EXIT 11
+#define LEVEL1 12
+#define LEVEL2 13
+#define LEVEL3 14
+
+static int msgID = 0;
+int status = MENU;
+int score;
+
+void *score_timer(void *data)
+{
+    while (1)
+    {
+        if (score < 0)
+            score = 0;
+        fndDisp(score, 0);
+        usleep(50);
+        score--;
+    }
+}
+
+void HW_init()
+{
+    //init_accel();
+    //init_gyro();
+    textLCD_Init();
+    ledInit();
+    printf("HW INIT\n");
+}
+
+void HW_close()
+{
+    close_accel();
+    textLCD_off();
+    ledExit();
+}
+
+int set_Mobility()
+{
+    int temp = readTEMP() / 5;
+    if (temp > 12)
+        temp = 12;
+    return temp;
+}
+
+void level_1_background(void)
+{
+    draw_background(0x8B4513, 0);
+    draw_square(50, 50, 1024 - 100, 600 - 100, 0xFFFFFF, 0);
+   
+}
+
+void level_2_background(void)
+{
+    draw_background(0x8B4513, 0);
+    draw_square(50, 50, 1024 - 100, 600 - 100, 0xFFFFFF, 0);
+    draw_square(358, 250, 308, 100, 0x8B4513, 0);
+}
+void level_3_background(void)
+{
+    draw_background(0x8B4513, 0);
+    draw_square(50, 50, 1024 - 100, 600 - 100, 0xFFFFFF, 0);
+    draw_square(258, 200, 150, 200, 0x8B4513, 0);
+    draw_square(566, 200, 150, 200, 0x8B4513, 0);
+}
+
+void level_1_obj_1(int OnOff)
+{
+    if (OnOff == 1)
+       draw_bmp_custom("trophy.bmp", 340, 220, 50, 50, 0);
+        //draw_square(340, 220, 50, 50, 0xFF0000, 0);
+    else if (OnOff == 0)
+        draw_square(340, 220, 50, 50, 0xFFFFFF, 0);
+    else
+        ;
+}
+
+void level_1_obj_2(int OnOff)
+{
+    if (OnOff == 1)
+        draw_square(730, 400, 50, 50, 0x0000FF, 0);
+    else if (OnOff == 0)
+        draw_square(730, 400, 50, 50, 0xFFFFFF, 0);
+    else
+        ;
+}
+
+void level_2_obj_1(int OnOff)
+{
+    if (OnOff == 1)
+        draw_square(850, 400, 50, 50, 0xFF0000, 0);
+    else if (OnOff == 0)
+        draw_square(850, 400, 50, 50, 0xFFFFFF, 0);
+    else
+        ;
+}
+
+void level_2_obj_2(int OnOff)
+{
+    if (OnOff == 1)
+        draw_square(900, 100, 50, 50, 0x00FF00, 0);
+    else if (OnOff == 0)
+        draw_square(900, 100, 50, 50, 0xFFFFFF, 0);
+    else
+        ;
+}
+
+void level_2_obj_3(int OnOff)
+{
+    if (OnOff == 1)
+        draw_square(440, 450, 50, 50, 0x0000FF, 0);
+    else if (OnOff == 0)
+        draw_square(440, 450, 50, 50, 0xFFFFFF, 0);
+    else
+        ;
+}
+
+void level_3_obj_1(int OnOff)
+{
+    if (OnOff == 1)
+        draw_square(870, 500, 50, 50, 0xFF0000, 0);
+    else if (OnOff == 0)
+        draw_square(870, 500, 50, 50, 0xFFFFFF, 0);
+    else
+        ;
+}
+
+void level_3_obj_2(int OnOff)
+{
+    if (OnOff == 1)
+        draw_square(120, 120, 50, 50, 0x00FF00, 0);
+    else if (OnOff == 0)
+        draw_square(120, 120, 50, 50, 0xFFFFFF, 0);
+    else
+        ;
+}
+
+void level_3_obj_3(int OnOff)
+{
+    if (OnOff == 1)
+        draw_square(512, 300, 50, 50, 0x0000FF, 0);
+    else if (OnOff == 0)
+        draw_square(512, 300, 50, 50, 0xFFFFFF, 0);
+    else
+        ;
+}
+
+int main(void)
+{
+    HW_init();
+    display_init();
+    int mobility = set_Mobility();
+    msgID = buttonInit();
+    int returnValue = 0;
+    BUTTON_MSG_T messageRxData;
+    int flag_safe = 1;
+    int flag_display_score = 0;
+    int orb_count = 0;
+    int Ax;
+    int Ay;
+    char buffer[17];
+    sprintf(buffer, "Difficulty: %d", mobility);
+    pthread_t thread;
+    int thread_id;
+
+    while (1)
+    {
+        switch (status)
+        {
+        case MENU: // 메인 메뉴
+            // 프레임버퍼에 메뉴 사진 밀어넣고 띄우기
+            draw_bmp_menu();
+            printf("Press Key\r\n");
+            writeLCD(1, "Game Menu       ");
+            writeLCD(2, "                ");
+            for (int i = 0; i < 8; i++)
+            {
+                ledOnOff(i, OFF);
+            }
+            returnValue = msgrcv(msgID, &messageRxData, sizeof(int), 0, 0); // 버튼 입력 받기
+            if (returnValue < 0)
+            {
+                printf("Receive Key Failed\r\n");
+                status = MENU;
+                break;
+            }
+            switch (messageRxData.keyInput)
+            {
+            case KEY_HOME: // 정상 게임 모드
+                printf("Home key\r\n");
+                printf("PLAY key\r\n");
+                status = LEVEL1;
+                break;
+            case KEY_BACK: // 디버그 게임 모드
+                printf("Back key\r\n");
+                printf("PLAY key\r\n");
+                mobility = 1;
+                status = LEVEL1;
+                break;
+            case KEY_VOLUMEDOWN: // 게임 종료
+                printf("Volume down key\r\n");
+                printf("PWR OFF\r\n");
+                status = EXIT;
+                break;
+            default: // 잘못된 키 입력
+                printf("KEY INPUT ERROR \r\n");
+                break;
+            }
+            break;
+
+        case WIN: // 게임 승리
+            // 프레임버퍼에 승리 사진 밀어넣고 띄우기
+            writeLCD(1, "You Won!        ");
+            writeLCD(2, "You Are Winner! ");
+            draw_bmp_win();
+            pthread_cancel(thread);
+            returnValue = msgrcv(msgID, &messageRxData, sizeof(int), 0, 0); // 버튼 입력 받기
+            if (returnValue < 0)
+            {
+                printf("Receive Key Failed\r\n");
+                status = WIN;
+                break;
+            }
+            if (messageRxData.keyInput == KEY_HOME)
+            {
+                status = MENU;
+            }
+            else if (messageRxData.keyInput == KEY_VOLUMEDOWN)
+            {
+                status = EXIT;
+            }
+            break;
+
+        case EXIT: // 게임 종료
+            writeLCD(1, "Good Bye        ");
+            writeLCD(2, "                ");
+            draw_background(0x000000, 0);
+            buttonExit();
+            return 0;
+
+        case LEVEL1: // 레벨 1 : 오브 2개, R>B 순서로 터치
+
+            score = 999999;
+            thread_id = pthread_create(&thread, NULL, score_timer, NULL); // 점수 계산 타이머 시작
+
+            writeLCD(1, "LEVEL1          ");
+            writeLCD(2, buffer);
+            printf("LEVEL1\r\n");
+            Ax = 512;
+            Ay = 300;
+            flag_safe = 1;
+            orb_count = 0;
+
+            level_1_background();
+            while (flag_safe)
+            {
+                init_accel();
+                double ax = read_accel(Y) / 163;
+                double ay = read_accel(X) / 163;
+                Ax = Ax + (ax * mobility);
+                Ay = Ay + (ay * mobility);
+
+                int Cx = (Ax + 20);
+                int Cy = (Ay + 20);
+
+                if (orb_count == 2)
+                {
+                    level_1_obj_1(OFF);
+                    level_1_obj_2(OFF);
+                    status = LEVEL2;
+                    flag_safe = 0;
+                }
+                else if (orb_count == 1)
+                {
+                    level_1_obj_1(OFF);
+                    level_1_obj_2(ON);
+                }
+                else
+                {
+                    level_1_obj_1(ON);
+                    level_1_obj_2(ON);
+                }
+
+                if (Cx > 50 && Cx < 1024 - 50 && Cy > 50 && Cy < 600 - 50)
+                {
+                }
+                else
+                {
+                    status = FAIL;
+                    flag_safe = 0;
+                }
+
+                if (orb_count == 0 && Cx > 340 && Cx < 340 + 50 && Cy > 220 && Cy < 220 + 50)
+                {
+                    orb_count = 1;
+                    ledOnOff(0, ON);
+                }
+                if (orb_count == 1 && Cx > 730 && Cx < 730 + 50 && Cy > 400 && Cy < 400 + 50)
+                {
+                    orb_count = 2;
+                    ledOnOff(1, ON);
+                }
+
+                draw_square(Ax, Ay, 40, 40, 0x000000, 0);
+                usleep(100000);
+                draw_square(Ax, Ay, 40, 40, 0xFFFFFF, 0);
+
+                printf("Coordinate: %d, %d\r\n", Cx, Cy);
+            }
+            break;
+
+        case LEVEL2: // 레벨 2 : R > G > B 순서로 터치
+            writeLCD(1, "LEVEL2          ");
+            writeLCD(2, buffer);
+            printf("LEVEL2\r\n");
+            Ax = 100;
+            Ay = 100;
+            flag_safe = 1;
+
+            level_2_background();
+            while (flag_safe)
+            {
+                init_accel();
+                double ax = read_accel(Y) / 163;
+                double ay = read_accel(X) / 163;
+                Ax = Ax + (ax * mobility);
+                Ay = Ay + (ay * mobility);
+
+                int Cx = (Ax + 20);
+                int Cy = (Ay + 20);
+
+                if (orb_count == 5)
+                {
+                    level_2_obj_1(OFF);
+                    level_2_obj_2(OFF);
+                    level_2_obj_3(OFF);
+                    status = LEVEL3;
+                    flag_safe = 0;
+                }
+                else if (orb_count == 4)
+                {
+                    level_2_obj_1(OFF);
+                    level_2_obj_2(OFF);
+                    level_2_obj_3(ON);
+                }
+                else if (orb_count == 3)
+                {
+                    level_2_obj_1(OFF);
+                    level_2_obj_2(ON);
+                    level_2_obj_3(ON);
+                }
+                else
+                {
+                    level_2_obj_1(ON);
+                    level_2_obj_2(ON);
+                    level_2_obj_3(ON);
+                }
+
+                if (Cx > 50 && Cx < 1024 - 50 && Cy > 50 && Cy < 600 - 50)
+                {
+                    if (Cx > 358 && Cx < 358 + 308 && Cy > 250 && Cy < 250 + 100)
+                    {
+                        status = FAIL;
+                        flag_safe = 0;
+                    }
+                }
+                else
+                {
+                    status = FAIL;
+                    flag_safe = 0;
+                }
+
+                if (orb_count == 2 && Cx > 850 && Cx < 850 + 50 && Cy > 400 && Cy < 400 + 50)
+                {
+                    orb_count = 3;
+                    ledOnOff(2, ON);
+                }
+                if (orb_count == 3 && Cx > 900 && Cx < 900 + 50 && Cy > 100 && Cy < 100 + 50)
+                {
+                    orb_count = 4;
+                    ledOnOff(3, ON);
+                }
+                if (orb_count == 4 && Cx > 440 && Cx < 440 + 50 && Cy > 450 && Cy < 450 + 50)
+                {
+                    orb_count = 5;
+                    ledOnOff(4, ON);
+                }
+
+                draw_square(Ax, Ay, 40, 40, 0x000000, 0);
+                usleep(100000);
+                draw_square(Ax, Ay, 40, 40, 0xFFFFFF, 0);
+
+                printf("Coordinate: %d, %d\r\n", Cx, Cy);
+            }
+            break;
+
+        case LEVEL3: // 레벨 3 : R > G > B 순서로 터치
+            writeLCD(1, "LEVEL3          ");
+            writeLCD(2, buffer);
+            printf("LEVEL3\r\n");
+            Ax = 100;
+            Ay = 100;
+            flag_safe = 1;
+
+            level_3_background();
+            while (flag_safe)
+            {
+                init_accel();
+                double ax = read_accel(Y) / 163;
+                double ay = read_accel(X) / 163;
+                Ax = Ax + (ax * mobility);
+                Ay = Ay + (ay * mobility);
+
+                int Cx = (Ax + 20);
+                int Cy = (Ay + 20);
+
+                if (orb_count == 8)
+                {
+                    level_3_obj_1(OFF);
+                    level_3_obj_2(OFF);
+                    level_3_obj_3(OFF);
+                    status = WIN;
+                    flag_safe = 0;
+                }
+                else if (orb_count == 7)
+                {
+                    level_3_obj_1(OFF);
+                    level_3_obj_2(OFF);
+                    level_3_obj_3(ON);
+                }
+                else if (orb_count == 6)
+                {
+                    level_3_obj_1(OFF);
+                    level_3_obj_2(ON);
+                    level_3_obj_3(ON);
+                }
+                else
+                {
+                    level_3_obj_1(ON);
+                    level_3_obj_2(ON);
+                    level_3_obj_3(ON);
+                }
+
+                if (Cx > 50 && Cx < 1024 - 50 && Cy > 50 && Cy < 600 - 50)
+                {
+                    if (Cx > 258 && Cx < 258 + 150 && Cy > 200 && Cy < 200 + 200)
+                    {
+                        status = FAIL;
+                        flag_safe = 0;
+                    }
+                    else if (Cx > 566 && Cx < 566 + 150 && Cy > 200 && Cy < 200 + 200)
+                    {
+                        status = FAIL;
+                        flag_safe = 0;
+                    }
+                }
+                else
+                {
+                    status = FAIL;
+                    flag_safe = 0;
+                }
+
+                if (orb_count == 5 && Cx > 870 && Cx < 870 + 50 && Cy > 500 && Cy < 500 + 50)
+                {
+                    orb_count = 6;
+                    ledOnOff(5, ON);
+                }
+                if (orb_count == 6 && Cx > 120 && Cx < 120 + 50 && Cy > 120 && Cy < 120 + 50)
+                {
+                    orb_count = 7;
+                    ledOnOff(6, ON);
+                }
+                if (orb_count == 7 && Cx > 512 && Cx < 512 + 50 && Cy > 300 && Cy < 300 + 50)
+                {
+                    orb_count = 8;
+                    ledOnOff(7, ON);
+                }
+
+                draw_square(Ax, Ay, 40, 40, 0x000000, 0);
+                usleep(100000);
+                draw_square(Ax, Ay, 40, 40, 0xFFFFFF, 0);
+
+                printf("Coordinate: %d, %d\r\n", Cx, Cy);
+            }
+            break;
+
+        case FAIL: // 실패할 경우 처음부터 시작하거나 게임 종료
+            writeLCD(2, "Failed          ");
+            draw_bmp_fail();
+            pthread_cancel(thread);
+            returnValue = msgrcv(msgID, &messageRxData, sizeof(int), 0, 0); // 버튼 입력 받기
+            if (returnValue < 0)
+            {
+                printf("Receive Key Failed\r\n");
+                status = WIN;
+                break;
+            }
+            if (messageRxData.keyInput == KEY_HOME)
+            {
+                status = MENU;
+            }
+            else if (messageRxData.keyInput == KEY_VOLUMEDOWN)
+            {
+                status = EXIT;
+            }
+            break;
+        }
+    }
+}
---
+++ project/display.c
@@ -0,0 +1,314 @@
+#include "display.h"
+
+int fb_fd0;
+int fb_fd1;
+struct fb_var_screeninfo fbvar;
+struct fb_fix_screeninfo fbfix;
+unsigned char *fb_mapped0;
+unsigned char *fb_mapped1;
+unsigned long *ptr0;
+unsigned long *ptr1;
+
+int screen_width;
+int screen_height;
+int bits_per_pixel;
+int line_length;
+
+int mem_size;
+
+    char *pData, *data;
+    char r, g, b;
+    unsigned long pixel;
+    int cols = 0, rows = 0;
+    unsigned long bmpdata[1280 * 800];
+    int k, t;
+    unsigned char *pfbmap;
+
+int display_init(void)
+{
+    // 커서 제거
+    int conFD = open("/dev/tty0", O_RDWR);
+    ioctl(conFD, KDSETMODE, KD_GRAPHICS);
+    close(conFD);
+
+    if (access(FBDEV_FILE0, F_OK))
+    {
+        printf("%s: access error\n", FBDEV_FILE0);
+        exit(1);
+    }
+
+    if ((fb_fd0 = open(FBDEV_FILE0, O_RDWR)) < 0)
+    {
+        printf("%s: open error\n", FBDEV_FILE0);
+        exit(1);
+    }
+
+    if (ioctl(fb_fd0, FBIOGET_VSCREENINFO, &fbvar))
+    {
+        printf("%s: ioctl error - FBIOGET_VSCREENINFO \n", FBDEV_FILE0);
+        exit(1);
+    }
+
+    if (ioctl(fb_fd0, FBIOGET_FSCREENINFO, &fbfix))
+    {
+        printf("%s: ioctl error - FBIOGET_FSCREENINFO \n", FBDEV_FILE0);
+        exit(1);
+    }
+
+    if (access(FBDEV_FILE1, F_OK))
+    {
+        printf("%s: access error\n", FBDEV_FILE1);
+        exit(1);
+    }
+
+    if ((fb_fd1 = open(FBDEV_FILE1, O_RDWR)) < 0)
+    {
+        printf("%s: open error\n", FBDEV_FILE1);
+        exit(1);
+    }
+
+    if (ioctl(fb_fd1, FBIOGET_VSCREENINFO, &fbvar))
+    {
+        printf("%s: ioctl error - FBIOGET_VSCREENINFO \n", FBDEV_FILE1);
+        exit(1);
+    }
+
+    if (ioctl(fb_fd1, FBIOGET_FSCREENINFO, &fbfix))
+    {
+        printf("%s: ioctl error - FBIOGET_FSCREENINFO \n", FBDEV_FILE1);
+        exit(1);
+    }
+
+    screen_width = fbvar.xres;
+    screen_height = fbvar.yres;
+    bits_per_pixel = fbvar.bits_per_pixel;
+    line_length = fbfix.line_length;
+
+    printf("screen_width : %d\n", screen_width);
+    printf("screen_height : %d\n", screen_height);
+    printf("bits_per_pixel : %d\n", bits_per_pixel);
+    printf("line_length : %d\n", line_length);
+
+    mem_size = screen_width * screen_height * 4;
+    fb_mapped0 = (unsigned char *)mmap(0, mem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd0, 0);
+    fb_mapped1 = (unsigned char *)mmap(0, mem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd1, 0);
+
+    if (fb_mapped0 < 0 || fb_mapped1 < 0)
+    {
+        printf("mmap error!\n");
+        exit(1);
+    }
+
+    return 0;
+}
+
+int draw_background(int color, int idx_buffer)
+{
+    if (idx_buffer == 0)
+    {
+        for (int i = 0; i < screen_height; i++)
+        {
+            for (int j = 0; j < screen_width; j++)
+            {
+                ptr0 = (unsigned long *)fb_mapped0 + screen_width * i + j;
+                *ptr0 = color;
+            }
+        }
+        return 0;
+    }
+    else if (idx_buffer == 1)
+    {
+        for (int i = 0; i < screen_height; i++)
+        {
+            for (int j = 0; j < screen_width; j++)
+            {
+                ptr1 = (unsigned long *)fb_mapped1 + screen_width * i + j;
+                *ptr1 = color;
+            }
+        }
+        return 0;
+    }
+    else
+        return -1;
+}
+
+int draw_square(int der_x, int der_y, int dX, int dY, int color, int idx_buffer)
+{
+    if (idx_buffer == 0)
+    {
+        for (int x = der_x; x < der_x + dX; x++)
+        {
+            for (int y = der_y; y < der_y + dY; y++)
+            {
+                ptr0 = (unsigned long *)fb_mapped0 + screen_width * y + x;
+                *ptr0 = color;
+            }
+        }
+        return 0;
+    }
+    else if (idx_buffer == 1)
+    {
+        for (int x = der_x; x < der_x + dX; x++)
+        {
+            for (int y = der_y; y < der_y + dY; y++)
+            {
+                ptr1 = (unsigned long *)fb_mapped1 + screen_width * y + x;
+                *ptr1 = color;
+            }
+        }
+        return 0;
+    }
+    else
+        return -1;
+}
+
+int draw_bmp_menu(void)
+{
+    read_bmp("MENU.bmp", &pData, &data, &cols, &rows);
+    for (int j = 0; j < rows; j++)
+    {
+        k = j * cols * 3;
+        t = (rows - 1 - j) * cols; // 가로 size가 작을 수도 있다.
+
+        for (int i = 0; i < cols; i++)
+        {
+            b = *(data + (k + i * 3));
+            g = *(data + (k + i * 3 + 1));
+            r = *(data + (k + i * 3 + 2));
+
+            pixel = ((r << 16) | (g << 8) | b);
+            bmpdata[t + i] = pixel; // save bitmap data bottom up
+        }
+    }
+    printf("1");
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            ptr0 = (unsigned long *)fb_mapped0 + cols * i + j;
+            *ptr0 = bmpdata[j + i * cols];
+        }
+    }
+    close_bmp(&pData);
+}
+
+int draw_bmp_win(void)
+{
+    read_bmp("WIN.bmp", &pData, &data, &cols, &rows);
+    for (int j = 0; j < rows; j++)
+    {
+        k = j * cols * 3;
+        t = (rows - 1 - j) * cols; // 가로 size가 작을 수도 있다.
+
+        for (int i = 0; i < cols; i++)
+        {
+            b = *(data + (k + i * 3));
+            g = *(data + (k + i * 3 + 1));
+            r = *(data + (k + i * 3 + 2));
+
+            pixel = ((r << 16) | (g << 8) | b);
+            bmpdata[t + i] = pixel; // save bitmap data bottom up
+        }
+    }
+    printf("1");
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            ptr0 = (unsigned long *)fb_mapped0 + cols * i + j;
+            *ptr0 = bmpdata[j + i * cols];
+        }
+    }
+    close_bmp(&pData);
+}
+
+int draw_bmp_fail(void)
+{
+    read_bmp("FAIL.bmp", &pData, &data, &cols, &rows);
+    for (int j = 0; j < rows; j++)
+    {
+        k = j * cols * 3;
+        t = (rows - 1 - j) * cols; // 가로 size가 작을 수도 있다.
+
+        for (int i = 0; i < cols; i++)
+        {
+            b = *(data + (k + i * 3));
+            g = *(data + (k + i * 3 + 1));
+            r = *(data + (k + i * 3 + 2));
+
+            pixel = ((r << 16) | (g << 8) | b);
+            bmpdata[t + i] = pixel; // save bitmap data bottom up
+        }
+    }
+    printf("1");
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            ptr0 = (unsigned long *)fb_mapped0 + cols * i + j;
+            *ptr0 = bmpdata[j + i * cols];
+        }
+    }
+    close_bmp(&pData);
+}
+int draw_bmp_custom(char *filename, int x, int y, int width, int height, int idx_buffer) {
+    char *pData, *data;
+    int cols, rows;
+    unsigned char r, g, b;
+    unsigned long pixel;
+    unsigned long bmpdata[1280 * 800];
+    int k, t;
+    unsigned long *ptr;
+
+    // Read BMP file
+    if (read_bmp(filename, &pData, &data, &cols, &rows) < 0) {
+        printf("File open failed\r\n");
+        return -1;
+    }
+
+    // Draw BMP at the specified position and size
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            // Calculate source pixel position
+            k = (i * rows / height) * cols * 3;
+            t = (rows - 1 - (i * rows / height)) * cols;
+
+            // Read RGB values
+            b = *(data + (k + (j * cols / width) * 3));
+            g = *(data + (k + (j * cols / width) * 3 + 1));
+            r = *(data + (k + (j * cols / width) * 3 + 2));
+
+            // Create pixel value
+            pixel = ((r << 16) | (g << 8) | b);
+            
+            // Determine framebuffer pointer based on the specified buffer index
+            if (idx_buffer == 0) {
+                ptr = (unsigned long *)fb_mapped0 + (y + i) * screen_width + (x + j);
+            } else if (idx_buffer == 1) {
+                ptr = (unsigned long *)fb_mapped1 + (y + i) * screen_width + (x + j);
+            } else {
+                printf("Invalid buffer index\r\n");
+                return -1;
+            }
+
+            // Write pixel to the framebuffer
+            *ptr = pixel;
+        }
+    }
+
+    // Close BMP file
+    close_bmp(&pData);
+
+    return 0;
+}
+int display_close(void)
+{
+    munmap(fb_mapped0, mem_size);
+    close(fb_fd0);
+    munmap(fb_mapped1, mem_size);
+    close(fb_fd1);
+    return 0;
+}
