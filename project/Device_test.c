cabb508fe21bf7941b4597129d762fa80f8b9148
Parkjaesung1 <su0000ng@naver.com>
2023-12-19 19:21:39 +0900

    수정

--- project/Device_test.c
+++ project/Device_test.c
@@ -32,7 +32,7 @@ static int obstacleY2_1=100;
 static int obstacleX2_2=250;
 static int obstacleY2_2=300;
 
-static int obstacleX3_1=750;
+static int obstacleX3_1=300;
 static int obstacleY3_1=100;
 static int obstacleX3_2=250;
 static int obstacleY3_2=400;
@@ -80,12 +80,12 @@ void level_1_obstacle(int OnOff)
     static int obstacleDirection = 1; // 1: 오른쪽, -1: 왼쪽
 
     if (OnOff == 1)
-        draw_bmp_custom("joat.bmp", obstacleX, obstacleY, 80, 80, 0);
+        draw_bmp_custom("joat.bmp", obstacleX, obstacleY, 65, 65, 0);
 
     else if (OnOff == 0)
         {
-        draw_square(obstacleX-10, obstacleY, 80, 80, 0xFFFFFF, 0);
-        draw_square(obstacleX+10, obstacleY, 80, 80, 0xFFFFFF, 0);
+        draw_square(obstacleX-10, obstacleY, 65, 65, 0xFFFFFF, 0);
+        draw_square(obstacleX+10, obstacleY, 65, 65, 0xFFFFFF, 0);
         }
     else
         ;
@@ -104,15 +104,15 @@ void level_2_obstacle(int OnOff)
     static int obstacleDirection2 = 1; // 1: 아래쪽, -1: 위쪽
     if (OnOff == 1)
 {        
-        draw_bmp_custom("joat.bmp", obstacleX2_1, obstacleY2_1, 80, 80, 0);
-        draw_bmp_custom("joat.bmp", obstacleX2_2, obstacleY2_2, 80, 80, 0);
+        draw_bmp_custom("joat.bmp", obstacleX2_1, obstacleY2_1, 65, 65, 0);
+        draw_bmp_custom("joat.bmp", obstacleX2_2, obstacleY2_2, 65, 65, 0);
 }
     else if (OnOff == 0)
         {
-        draw_square(obstacleX2_1, obstacleY2_1-10, 80, 80, 0xFFFFFF, 0);
-        draw_square(obstacleX2_1, obstacleY2_1+10, 80, 80, 0xFFFFFF, 0);
-        draw_square(obstacleX2_2, obstacleY2_2-10, 80, 80, 0xFFFFFF, 0);
-        draw_square(obstacleX2_2, obstacleY2_2+10, 80, 80, 0xFFFFFF, 0);
+        draw_square(obstacleX2_1, obstacleY2_1-10, 65, 65, 0xFFFFFF, 0);
+        draw_square(obstacleX2_1, obstacleY2_1+10, 65, 65, 0xFFFFFF, 0);
+        draw_square(obstacleX2_2, obstacleY2_2-10, 65, 65, 0xFFFFFF, 0);
+        draw_square(obstacleX2_2, obstacleY2_2+10, 65, 65, 0xFFFFFF, 0);
         }
     else
         ;
@@ -131,23 +131,23 @@ void level_2_obstacle(int OnOff)
 void level_3_obstacle(int OnOff)
 {
 
-    static int obstacleDirection1 = 1; // 1: 아래쪽, -1: 위쪽
-    static int obstacleDirection2 = 1; // 1: 아래쪽, -1: 위쪽
+    static int obstacleDirection1 = -1; // 1: 오른쪽, -1: 왼쪽
+    static int obstacleDirection2 = 1; // 1: 오른쪽, -1: 왼쪽
     static int obstacleDirection3 = 1; // 1: 아래쪽, -1: 위쪽
     if (OnOff == 1)
         {        
-        draw_bmp_custom("joat.bmp", obstacleX3_1, obstacleY3_1, 80, 80, 0);
-        draw_bmp_custom("joat.bmp", obstacleX3_2, obstacleY3_2, 80, 80, 0);
-        draw_bmp_custom("joat.bmp", obstacleX3_3, obstacleY3_3, 80, 80, 0);
+        draw_bmp_custom("joat.bmp", obstacleX3_1, obstacleY3_1, 65, 65, 0);
+        draw_bmp_custom("joat.bmp", obstacleX3_2, obstacleY3_2, 65, 65, 0);
+        draw_bmp_custom("joat.bmp", obstacleX3_3, obstacleY3_3, 65, 65, 0);
         }
     else if (OnOff == 0)
         {
-        draw_square(obstacleX3_1+10, obstacleY3_1, 80, 80, 0xFFFFFF, 0);
-        draw_square(obstacleX3_1-10, obstacleY3_1, 80, 80, 0xFFFFFF, 0);
-        draw_square(obstacleX3_2+10, obstacleY3_2, 80, 80, 0xFFFFFF, 0);
-        draw_square(obstacleX3_2-10, obstacleY3_2, 80, 80, 0xFFFFFF, 0);
-        draw_square(obstacleX3_3, obstacleY3_3-10, 80, 80, 0xFFFFFF, 0);
-        draw_square(obstacleX3_3, obstacleY3_3+10, 80, 80, 0xFFFFFF, 0);
+        draw_square(obstacleX3_1+10, obstacleY3_1, 65, 65, 0xFFFFFF, 0);
+        draw_square(obstacleX3_1-10, obstacleY3_1, 65, 65, 0xFFFFFF, 0);
+        draw_square(obstacleX3_2+10, obstacleY3_2, 65, 65, 0xFFFFFF, 0);
+        draw_square(obstacleX3_2-10, obstacleY3_2, 65, 65, 0xFFFFFF, 0);
+        draw_square(obstacleX3_3, obstacleY3_3-10, 65, 65, 0xFFFFFF, 0);
+        draw_square(obstacleX3_3, obstacleY3_3+10, 65, 65, 0xFFFFFF, 0);
         
         }
     else
@@ -163,7 +163,7 @@ void level_3_obstacle(int OnOff)
         obstacleDirection1 *= -1;
     if(obstacleX3_2 <= 70 || obstacleX3_2 >= 1024 - 150)
         obstacleDirection2 *= -1;
-    if (obstacleY3_3 <= 150 || obstacleY3_3 >= 600 - 150) 
+    if (obstacleY3_3 <= 100 || obstacleY3_3 >= 600 - 150) 
         obstacleDirection3 *= -1;
     
 }
@@ -171,7 +171,7 @@ void level_3_obstacle(int OnOff)
 void level_1_background(void)
 {
     
-    draw_background(0x8B4513, 0);
+    draw_background(0x000000, 0);
     draw_square(50, 50, 1024 - 100, 600 - 100, 0xFFFFFF, 0);
     
     level_1_obstacle(1);
@@ -182,16 +182,16 @@ void level_2_background(void)
 {
     draw_background(0x000000, 0);
     draw_square(50, 50, 1024 - 100, 600 - 100, 0xFFFFFF, 0);
-    draw_square(358, 250, 308, 100, 0x8B4513, 0);
+    draw_square(358, 250, 308, 100, 0x000000, 0);
     level_2_obstacle(1);
 }
 
 void level_3_background(void)
 {
-    draw_background(0x8B4513, 0);
+    draw_background(0x000000, 0);
     draw_square(50, 50, 1024 - 100, 600 - 100, 0xFFFFFF, 0);
-    draw_square(258, 200, 150, 200, 0x8B4513, 0);
-    draw_square(566, 200, 150, 200, 0x8B4513, 0);
+    draw_square(258, 200, 150, 200, 0x000000, 0);
+    draw_square(566, 200, 150, 200, 0x000000, 0);
     level_3_obstacle(1);
 }
 void level_1_update(void)
@@ -220,7 +220,6 @@ void level_1_obj_1(int OnOff)
 {
     if (OnOff == 1)
       draw_bmp_custom("trophy.bmp", 340, 220, 50, 50, 0);
-    //draw_square(340, 220, 50, 50, 0xFF0000, 0);
        
     else if (OnOff == 0)
 {
@@ -233,8 +232,7 @@ void level_1_obj_1(int OnOff)
 void level_1_obj_2(int OnOff)
 {
     if (OnOff == 1)      
-        draw_bmp_custom("hou.bmp", 730, 400, 50, 50, 0);        
-        //draw_square(730, 400, 50, 50, 0x0000FF, 0);
+        draw_bmp_custom("champs.bmp", 730, 400, 50, 50, 0);        
     else if (OnOff == 0)
         draw_square(730, 400, 50, 50, 0xFFFFFF, 0);
     else
@@ -244,7 +242,7 @@ void level_1_obj_2(int OnOff)
 void level_2_obj_1(int OnOff)
 {
     if (OnOff == 1)
-        draw_square(850, 400, 50, 50, 0xFF0000, 0);
+        draw_bmp_custom("champs.bmp", 850, 400, 50, 50, 0);
     else if (OnOff == 0)
         draw_square(850, 400, 50, 50, 0xFFFFFF, 0);
     else
@@ -254,9 +252,9 @@ void level_2_obj_1(int OnOff)
 void level_2_obj_2(int OnOff)
 {
     if (OnOff == 1)
-        draw_square(900, 100, 50, 50, 0x00FF00, 0);
+        draw_bmp_custom("trophy.bmp", 600, 100, 50, 50, 0);
     else if (OnOff == 0)
-        draw_square(900, 100, 50, 50, 0xFFFFFF, 0);
+        draw_square(600, 100, 50, 50, 0xFFFFFF, 0);
     else
         ;
 }
@@ -264,7 +262,7 @@ void level_2_obj_2(int OnOff)
 void level_2_obj_3(int OnOff)
 {
     if (OnOff == 1)
-        draw_square(440, 450, 50, 50, 0x0000FF, 0);
+        draw_bmp_custom("gold.bmp", 440, 450, 50, 50, 0);
     else if (OnOff == 0)
         draw_square(440, 450, 50, 50, 0xFFFFFF, 0);
     else
@@ -274,9 +272,9 @@ void level_2_obj_3(int OnOff)
 void level_3_obj_1(int OnOff)
 {
     if (OnOff == 1)
-        draw_square(870, 500, 50, 50, 0xFF0000, 0);
+        draw_bmp_custom("champs.bmp", 870, 250, 50, 50, 0);
     else if (OnOff == 0)
-        draw_square(870, 500, 50, 50, 0xFFFFFF, 0);
+        draw_square(870, 400, 50, 50, 0xFFFFFF, 0);
     else
         ;
 }
@@ -284,7 +282,7 @@ void level_3_obj_1(int OnOff)
 void level_3_obj_2(int OnOff)
 {
     if (OnOff == 1)
-        draw_square(120, 120, 50, 50, 0x00FF00, 0);
+        draw_bmp_custom("gold.bmp", 120, 120, 50, 50, 0);
     else if (OnOff == 0)
         draw_square(120, 120, 50, 50, 0xFFFFFF, 0);
     else
@@ -294,7 +292,7 @@ void level_3_obj_2(int OnOff)
 void level_3_obj_3(int OnOff)
 {
     if (OnOff == 1)
-        draw_square(512, 300, 50, 50, 0x0000FF, 0);
+        draw_bmp_custom("trophy.bmp", 512, 300, 50, 50, 0);
     else if (OnOff == 0)
         draw_square(512, 300, 50, 50, 0xFFFFFF, 0);
     else
@@ -452,7 +450,7 @@ int main(void)
                     level_1_obj_2(ON);
                 }
 
-                if (Cx > 50 && Cx < 1024 - 50 && Cy > 50 && Cy < 600 - 50)
+                if (Cx > 80 && Cx < 1024 - 80 && Cy > 80 && Cy < 600 - 80)
                 {
                 }
                 else
@@ -461,24 +459,24 @@ int main(void)
                     flag_safe = 0;
                 }
 
-                if (orb_count == 0 && Cx > 340 && Cx < 340 + 50 && Cy > 220 && Cy < 220 + 50)
+                if (orb_count == 0 && Cx > 340-10 && Cx < 340 + 80 && Cy > 220-10 && Cy < 220 + 80)
                 {
                     orb_count = 1;
                     orb_case = 1;
                     ledOnOff(0, ON);
                 }
-                if (orb_count == 0 && Cx > 730 && Cx < 730 + 50 && Cy > 400 && Cy < 400 + 50)
+                if (orb_count == 0 && Cx > 730-10 && Cx < 730 + 80 && Cy > 400-10 && Cy < 400 + 80)
                 {
                     orb_count = 1;
                     orb_case = 2;
                     ledOnOff(0, ON);
                 }
-                if (orb_count == 1 && Cx > 340 && Cx < 340 + 50 && Cy > 220 && Cy < 220 + 50 && orb_case ==2)
+                if (orb_count == 1 && Cx > 340-10 && Cx < 340 + 80 && Cy > 220-10 && Cy < 220 + 80 && orb_case ==2)
                 {
                     orb_count = 2;
                     ledOnOff(1, ON);
                 }
-                if (orb_count == 1 && Cx > 730 && Cx < 730 + 50 && Cy > 400 && Cy < 400 + 50 && orb_case ==1)
+                if (orb_count == 1 && Cx > 730-10 && Cx < 730 + 80 && Cy > 400-10 && Cy < 400 + 80 && orb_case ==1)
                 {
                     orb_count = 2;
                     ledOnOff(1, ON);
@@ -572,7 +570,7 @@ case LEVEL2: // 레벨 2 : R > G > B 순서로 터치
             level_2_obj_3(ON);
         }
 
-        if (Cx > 50 && Cx < 1024 - 50 && Cy > 50 && Cy < 600 - 50)
+        if (Cx > 80 && Cx < 1024 - 80 && Cy > 80 && Cy < 600 - 80)
         {
             if (Cx > 358 && Cx < 358 + 308 && Cy > 250 && Cy < 250 + 100)
             {
@@ -586,7 +584,7 @@ case LEVEL2: // 레벨 2 : R > G > B 순서로 터치
             flag_safe = 0;
         }
 
-        if (level2_orb1_trigger == 0 && Cx > 850 && Cx < 850 + 50 && Cy > 400 && Cy < 400 + 50)
+        if (level2_orb1_trigger == 0 && Cx > 850-10 && Cx < 850 + 80 && Cy > 400-10 && Cy < 400 + 80)
         {
             level2_orb1_trigger = 1;
             if (orb_count == 0)
@@ -596,7 +594,7 @@ case LEVEL2: // 레벨 2 : R > G > B 순서로 터치
             else if (orb_count == 2)
                 orb_count = 3;
         }
-        if (level2_orb2_trigger == 0 && Cx > 900 && Cx < 900 + 50 && Cy > 100 && Cy < 100 + 50)
+        if (level2_orb2_trigger == 0 && Cx > 600-10 && Cx < 600 + 80 && Cy > 100-10 && Cy < 100 + 80)
         {
             level2_orb2_trigger = 1;
             if (orb_count == 0)
@@ -606,7 +604,7 @@ case LEVEL2: // 레벨 2 : R > G > B 순서로 터치
             else if (orb_count == 2)
                 orb_count = 3;
         }
-        if (level2_orb3_trigger == 0 && Cx > 440 && Cx < 440 + 50 && Cy > 450 && Cy < 450 + 50)
+        if (level2_orb3_trigger == 0 && Cx > 440-10 && Cx < 440 + 80 && Cy > 450-10 && Cy < 450 + 80)
         {
             level2_orb3_trigger = 1;
             if (orb_count == 0)
@@ -617,8 +615,7 @@ case LEVEL2: // 레벨 2 : R > G > B 순서로 터치
                 orb_count = 3;
         }
 
-               draw_bmp_custom("goat.bmp", Ax, Ay, 80 , 80, 0);
-                //draw_square(Ax, Ay, 40, 40, 0x000000, 0);
+                draw_bmp_custom("goat.bmp", Ax, Ay, 80 , 80, 0);
                 level_2_update(); // 장애물 이동 및 그리기
                 usleep(100000);
                 draw_square(Ax, Ay, 80, 80, 0xFFFFFF, 0);
@@ -632,7 +629,7 @@ case LEVEL2: // 레벨 2 : R > G > B 순서로 터치
             writeLCD(2, buffer);
             printf("LEVEL3\r\n");
             Ax = 100;
-            Ay = 100;
+            Ay = 300;
             flag_safe = 1;
     	     int level3_orb1_trigger = 0, level3_orb2_trigger = 0, level3_orb3_trigger = 0;
    	        orb_count = 0;
@@ -713,7 +710,7 @@ case LEVEL2: // 레벨 2 : R > G > B 순서로 터치
                     level_3_obj_3(ON);
                 }
 
-                if (Cx > 50 && Cx < 1024 - 50 && Cy > 50 && Cy < 600 - 50)
+                if (Cx > 50 && Cx < 1024 - 80 && Cy > 80 && Cy < 600 - 80)
                 {
                     if (Cx > 258 && Cx < 258 + 150 && Cy > 200 && Cy < 200 + 200)
                     {
@@ -732,7 +729,7 @@ case LEVEL2: // 레벨 2 : R > G > B 순서로 터치
                     flag_safe = 0;
                 }
 
-                if (level3_orb1_trigger == 0 && Cx > 870 && Cx < 870 + 50 && Cy > 500 && Cy < 500 + 50)
+                if (level3_orb1_trigger == 0 && Cx > 870-10 && Cx < 870 + 80 && Cy > 250-10 && Cy < 250 + 80)
                 {
             level3_orb1_trigger = 1;
             if (orb_count == 0)
@@ -742,7 +739,7 @@ case LEVEL2: // 레벨 2 : R > G > B 순서로 터치
             else if (orb_count == 2)
                 orb_count = 3;
                 }
-                if (level3_orb2_trigger == 0 && Cx > 120 && Cx < 120 + 50 && Cy > 120 && Cy < 120 + 50)
+                if (level3_orb2_trigger == 0 && Cx > 120-10 && Cx < 120 + 80 && Cy > 120-10 && Cy < 120 + 80)
                 {
             level3_orb2_trigger = 1;
             if (orb_count == 0)
@@ -752,7 +749,7 @@ case LEVEL2: // 레벨 2 : R > G > B 순서로 터치
             else if (orb_count == 2)
                 orb_count = 3;
                 }
-                if (level3_orb3_trigger == 0 && Cx > 512 && Cx < 512 + 50 && Cy > 300 && Cy < 300 + 50)
+                if (level3_orb3_trigger == 0 && Cx > 512-10 && Cx < 512 + 80 && Cy > 300-10 && Cy < 300 + 80)
                 {
             level3_orb3_trigger = 1;
             if (orb_count == 0)
@@ -763,10 +760,10 @@ case LEVEL2: // 레벨 2 : R > G > B 순서로 터치
                 orb_count = 3;
                 }
 
-                draw_square(Ax, Ay, 40, 40, 0x000000, 0);
+                draw_bmp_custom("goat.bmp", Ax, Ay, 75 , 75, 0);
                 level_3_update();
                 usleep(100000);
-                draw_square(Ax, Ay, 40, 40, 0xFFFFFF, 0);
+                draw_square(Ax, Ay, 75, 75, 0xFFFFFF, 0);
 
                 printf("Coordinate: %d, %d\r\n", Cx, Cy);
             }
